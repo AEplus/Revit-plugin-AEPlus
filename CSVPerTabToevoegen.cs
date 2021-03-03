9414ea4bc2bc3d1641774bce7f1062cb0d7715e2
sbellinkx <39960806+sbellinkx@users.noreply.github.com>
2021-03-03 15:09:20 +0100

    Michiel update 02/03 15u

---
+++ CSVPerTabToevoegen.cs
@@ -0,0 +1,88 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using System;
+using xlApp = Microsoft.Office.Interop.Excel;
+using Microsoft.Office.Interop.Excel;
+using System.Runtime.InteropServices;
+using System.IO;
+using OfficeOpenXml;
+
+namespace MyRevitCommands
+{
+    [TransactionAttribute(TransactionMode.ReadOnly)]
+    public class CSVPerTabToevoegen : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+
+            Result r = Result.Failed;
+            xlApp.Application ExcelApp = null;
+            xlApp.Workbook workbook = null;
+            xlApp.Sheets sheets = null;
+            xlApp.Worksheet newSheet = null;
+
+            // MisValue for excel files, unknown values
+            object misValue = System.Reflection.Missing.Value;
+
+            //Get UIDocument
+            UIDocument uidoc = commandData.Application.ActiveUIDocument;
+            //Get Document
+            Document doc = uidoc.Document;
+
+            FilteredElementCollector col = new FilteredElementCollector(doc)
+            .OfClass(typeof(ViewSchedule));
+
+            /*
+           Export Options on how the .txt file will look. Text "" is gone
+           FieldDelimiter is TAB replaced with ,  
+            */
+            ViewScheduleExportOptions opt = new ViewScheduleExportOptions
+            {
+                TextQualifier = ExportTextQualifier.None,
+                FieldDelimiter = ","
+            };
+
+            //Instantiate the Application object.
+            xlApp.Application excelApp = new xlApp.Application();
+
+            //Open the excel file.
+            workbook = excelApp.Workbooks.Open(@"c:\\temp\\test.xlsx", 0, false, 5, "", "",
+                           false, XlPlatform.xlWindows, "",
+                           true, false, 0, true, false, false);
+
+            //Declare a Worksheet object.
+            sheets = workbook.Sheets as Sheets;
+
+            int i = 1;
+            foreach (ViewSchedule vs in col)
+            {
+                vs.Export(@"c:\\temp\\test", Environment.UserName + vs.Name + ".csv", opt);
+
+                newSheet = (Worksheet)sheets.Add(sheets[i], Type.Missing, Type.Missing, Type.Missing);
+
+
+
+
+                // workbook.ActiveSheet(@"c:\\temp\\test\" + vs.Name + ".csv");
+                //   newSheet.Name = vs.Name;
+                //   workbook.Save();
+
+
+
+            }
+
+            workbook.Close(Type.Missing, Type.Missing, Type.Missing);
+                ExcelApp.Quit();
+                Marshal.ReleaseComObject(newSheet);
+                Marshal.ReleaseComObject(sheets);
+                Marshal.ReleaseComObject(workbook);
+                Marshal.ReleaseComObject(excelApp);
+                excelApp = null;
+
+                return r;
+           
+            return r;
+        }
+    } 
+}
--- ExportSchedules.cs
+++ ExportSchedules.cs
@@ -1,39 +1,168 @@
-﻿using Autodesk.Revit.Attributes;
-using Autodesk.Revit.DB;
-using Autodesk.Revit.UI;
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//using Autodesk.Revit.Attributes;
+//using Autodesk.Revit.DB;
+//using Autodesk.Revit.UI;
+//using Microsoft.VisualBasic.FileIO;
+//using System;
+//using System.Collections.Generic;
+//using System.IO;
+//using System.Windows.Forms;
+//using Excel = Microsoft.Office.Interop.Excel;
 
-namespace MyRevitCommands
-{
-    [TransactionAttribute(TransactionMode.ReadOnly)]
-    public class ExportingSchedules : IExternalCommand
-    {
-        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
-        {
+//namespace MyRevitCommands
+//{
+//    [TransactionAttribute(TransactionMode.ReadOnly)]
+//    public class ExportSchedule : IExternalCommand
+//    {
+//        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+//        {
+//            // Get UIDocument
+//            UIDocument uidoc = commandData.Application.ActiveUIDocument;
+//            // Get Document
+//            Document doc = uidoc.Document;
+//            FilteredElementCollector col = new FilteredElementCollector(doc)
+//                                                .OfClass(typeof(ViewSchedule));
+//            /*
+//            Export Options on how the .txt file will look. Text "" is gone
+//            FieldDelimiter is TAB replaced with ,  
+//             */
+//            ViewScheduleExportOptions opt = new ViewScheduleExportOptions
+//            {
+//                TextQualifier = ExportTextQualifier.None,
+//                FieldDelimiter = ","
+//            };
 
-            //Get UIDocument
-            UIDocument uidoc = commandData.Application.ActiveUIDocument;
+//            // MisValue for excel files, unknown values
+//            object misValue = System.Reflection.Missing.Value;
 
-            //Get Document
-            Document doc = uidoc.Document;
+//            // Launch Excel application and check if the application is available 
+//            Excel.Application xlApp = new
+//            Microsoft.Office.Interop.Excel.Application();
 
-            FilteredElementCollector col = new FilteredElementCollector(doc)
-            .OfClass(typeof(ViewSchedule));
+//            if (xlApp == null)
+//            {
+//                MessageBox.Show("Excel is not properly installed!");
+//            }
 
-            ViewScheduleExportOptions opt = new ViewScheduleExportOptions();
+//            foreach (ViewSchedule vs in col)
+//            {
+//                Directory.CreateDirectory(@"c:\\temp");
+//                vs.Export(@"c:\\temp", Environment.UserName + DateTime.Today + vs.Name + ".csv", opt);
 
-            foreach (ViewSchedule vs in col)
-            {
-                Directory.CreateDirectory(@"c:\\temp\\test");
-                vs.Export(@"c:\\temp\\test", vs.Name + ".csv", opt);
-            }
+//                // search temp folder
+//                var ListOfcsvFilenames = Directory.GetFiles("c:\\temp", Environment.UserName + "*.csv");
 
-            return Result.Succeeded;
-        }
-    }
-}
+//                // Copy files and move to temp folder 
+
+//                // read newest csv files to array
+//                List<CustomSchedule> customSchedules = new List<CustomSchedule>();
+//                for (csvFileNames in ListOfcsvFilenames)
+//                {
+//                    customSchedules.Add(new CustomSchedule(File.ReadAllLines(csvFileNames.ToString())));
+//                }
+
+//                // making excel
+
+
+//                // create excel file
+//                Excel.Application oXL = new Excel.Application();
+//                oXL.Visible = false;
+//                Excel.Workbook oWB = oXL.Workbooks.Add(misValue);
+
+//                // Excel inladen
+//                foreach (CustomSchedule cs in customSchedules)
+//                {
+//                    // create excel sheet
+//                    Excel.Worksheet oSheet = oWB.Sheets.Add(misValue,
+//                                                            misValue,
+//                                                            1,
+//                                                            misValue)
+//                                                            as Excel.Worksheet;
+
+//                    // add excel sheet name
+//                    oSheet.Name = cs.getTitle();
+//                    oSheet.Cells[1, 1] = cs.getTitle();
+
+//                    // add excel sheet headers
+//                    int i = 1;
+//                    foreach (string header in cs.getHeaders())
+//                    {
+//                        oSheet.Cells[2, i] = header;
+//                        i++;
+//                    }
+
+//                    // looping over an excel row that will be filled in with data from 
+//                    // CustomSchedule object 
+//                    int y = 3;
+//                    foreach (List<string> row in cs.getData())
+//                    {
+//                        // this is an excel row
+//                        int x = 1;
+//                        foreach (string item in row)
+//                        {
+//                            // this is an excel row column
+//                            oSheet.Cells[x, y] = item;
+//                            x++;
+//                        }
+//                        y++;
+//                    }
+//                }
+
+//                // Save project to path, close and save changes, quit excel 
+//                oWB.SaveAs("C:\\test1\test1.xls", Excel.XlFileFormat.xlWorkbookNormal, misValue, misValue, misValue, misValue, Excel.XlSaveAsAccessMode.xlExclusive, misValue, misValue, misValue, misValue, misValue);
+//                oWB.Close(true, misValue, misValue);
+//                oXL.UserControl = true;
+//                oXL.Quit();
+//            }
+//            return Result.Succeeded;
+//        }
+//    }
+//    public class CustomSchedule
+//    {
+//        private string title;
+//        private List<string> headers;
+//        private List<List<string>> data;
+//        public CustomSchedule(string[] data)
+//        {
+//            this.data = new List<List<string>>();
+//            for (int i = 0; i < data.Length; i++)
+//            {
+//                Console.Out.WriteLine(data[i]);
+//                if (i == 0)
+//                {
+//                    // read title from file
+//                    this.title = data[i];
+//                }
+//                else if (i == 1)
+//                {
+//                    // read headers
+//                    this.headers = new List<string>(data[i].Split(','));
+//                }
+//                else
+//                {
+//                    // read rows 
+//                    this.data.Add(
+//                            new List<string>(data[i].Split(',')));
+//                }
+//            }
+//        }
+//        // Excel title, headers and the rest of the data 
+//        public CustomSchedule(string title, List<string> headers, List<List<string>> data)
+//        {
+//            this.title = title;
+//            this.headers = headers;
+//            this.data = data;
+//        }
+//        public string getTitle()
+//        {
+//            return this.title;
+//        }
+//        public List<string> getHeaders()
+//        {
+//            return this.headers;
+//        }
+//        public List<List<string>> getData()
+//        {
+//            return this.data;
+//        }
+//    }
+//}

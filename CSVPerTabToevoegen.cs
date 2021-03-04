fcc50aba2b61e998cfd32c45f814239802885a8b
sbellinkx <39960806+sbellinkx@users.noreply.github.com>
2021-03-04 11:47:16 +0100

    Changes to excelpackage

--- CSVPerTabToevoegen.cs
+++ CSVPerTabToevoegen.cs
@@ -3,14 +3,9 @@ using Autodesk.Revit.DB;
 using Autodesk.Revit.UI;
 using System;
 using xlApp = Microsoft.Office.Interop.Excel;
-using Microsoft.Office.Interop.Excel;
 using System.Runtime.InteropServices;
-using System.IO;
 using OfficeOpenXml;
-using System.Text;
-using System.Globalization;
-using System.Threading;
-using Microsoft.IO;
+using System.IO;
 
 namespace MyRevitCommands
 {
@@ -18,8 +13,7 @@ namespace MyRevitCommands
     public class CSVPerTabToevoegen : IExternalCommand
     {
         public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
-        {
-
+        {         
             Result r = Result.Succeeded;
             xlApp.Application ExcelApp = null;
             xlApp.Workbook workbook = null;
@@ -41,79 +35,85 @@ namespace MyRevitCommands
            Export Options on how the .txt file will look. Text "" is gone
            FieldDelimiter is TAB replaced with ,  
             */
-            ViewScheduleExportOptions opt = new ViewScheduleExportOptions
-            {
-                TextQualifier = ExportTextQualifier.None,
-                FieldDelimiter = ","
-            };
+            ViewScheduleExportOptions opt = new ViewScheduleExportOptions();
+            //{
+            //    TextQualifier = ExportTextQualifier.None,
+            //    FieldDelimiter = ","
+            //};
 
-            //set the formatting options
-            ExcelTextFormat format = new ExcelTextFormat();
-            format.Delimiter = ';';
-            format.Culture = new CultureInfo(Thread.CurrentThread.CurrentCulture.ToString());
-            format.Culture.DateTimeFormat.ShortDatePattern = "dd-mm-yyyy";
-            format.Encoding = new UTF8Encoding();
-
-            ////Instantiate the Application object.
-            //xlApp.Application excelApp = new xlApp.Application();
-
-            ////Open the excel file.
-            //workbook = excelApp.Workbooks.Open(@"c:\\temp\\test.xlsx", 0, false, 5, "", "",
-            //               false, XlPlatform.xlWindows, "",
-            //               true, false, 0, true, false, false)
-
-            //create a new Excel package
-
+            
 
             foreach (ViewSchedule vs in col)
             {
-                vs.Export(@"c:\\temp\\test", Environment.UserName + vs.Name + ".csv", opt);
+                if (vs.Name.Contains("AE")){
+                    vs.Export(@"c:\\temp\", Environment.UserName + vs.Name + ".csv", opt);
 
-                //read the CSV file from disk
-                FileInfo file = new FileInfo(@"c:\\temp\\test" + Environment.UserName + vs.Name + ".csv");
 
-                var newFile = (@"c:\\temp\\test" + Environment.UserName + vs.Name + ".csv");
+                    FileInfo file = new FileInfo(@"c:\\temp\" + Environment.UserName + vs.Name + ".csv");
+                    //create a new Excel package
+                    using (ExcelPackage excel = new ExcelPackage())
+                    {
+                        //create a WorkSheet
+                        ExcelWorksheet worksheet = excel.Workbook.Worksheets.Add(vs.Name);
 
-                using (ExcelPackage excelPackage = new ExcelPackage())
-                {
-                    //create a WorkSheet
-                    ExcelWorksheet worksheet = excelPackage.Workbook.Worksheets.Add(vs.Name);
+                        ExcelTextFormat format = new ExcelTextFormat();
 
-                    //load the CSV data into cell A1
-                    worksheet.Cells["A1"].LoadFromText(file, format);
+                        //load the CSV data into cell A1
+                        worksheet.Cells["A1"].LoadFromText(file, format);
 
-                    // file name with .xlsx extension  
-                    string p_strPath = @"c:\\temp\\test.xlsx";
-
-                    if (File.Exists(p_strPath))
-                        File.Delete(p_strPath);
-
-                    // Create excel file on physical disk  
-                    FileStream objFileStrm = File.Create(p_strPath);
-                    objFileStrm.Close();
-
-                    // Write content to excel file  
-                    File.WriteAllBytes(p_strPath, excelPackage.GetAsByteArray());
-                    //Close Excel package 
-                    excelPackage.Dispose();
+                        //the path of the file
+                        string filePath = "C:\\temp\\ExcelDemo.xlsx";
+                        //Write the file to the disk
+                        FileInfo fi = new FileInfo(filePath);
+                        excel.SaveAs(fi);
+                    }
                 }
 
-                //newSheet = (Worksheet)sheets.Add(sheets[i], Type.Missing, Type.Missing, Type.Missing);
+                // read the CSV file from disk
+                // FileInfo file = new FileInfo(@"c:\\temp\\test" + Environment.UserName + vs.Name + ".csv");
 
-                // workbook.ActiveSheet(@"c:\\temp\\test\" + vs.Name + ".csv");
-                //   newSheet.Name = vs.Name;
-                //   workbook.Save();
+                // var newFile = (@"c:\\temp\\test" + Environment.UserName + vs.Name + ".csv");
+
+
+                // DIT IS EPPlus package 
+                //using (ExcelPackage excelPackage = new ExcelPackage())
+                //{
+                //    //create a WorkSheet
+                //    ExcelWorksheet worksheet = excelPackage.Workbook.Worksheets.Add(vs.Name);
+
+                //    //load the CSV data into cell A1
+                //    worksheet.Cells["A1"].LoadFromText(file, format);
+
+                //    // file name with .xlsx extension  
+                //    string p_strPath = @"c:\\temp\\test.xlsx";
+
+                //    if (File.Exists(p_strPath))
+                //        File.Delete(p_strPath);
+
+                //    // Create excel file on physical disk  
+                //    FileStream objFileStrm = File.Create(p_strPath);
+                //    objFileStrm.Close();
+
+                //    // Write content to excel file  
+                //    File.WriteAllBytes(p_strPath, excelPackage.GetAsByteArray());
+                //    //Close Excel package 
+                //    excelPackage.Dispose();
+                // TOT HIER IS HET EPPlus Package
+
             }
+            //newSheet = (Worksheet)sheets.Add(sheets[i], Type.Missing, Type.Missing, Type.Missing);
 
-            //workbook.SaveAs(@"c:\\temp\\DIKKETEST", ".xlsx");
-            //workbook.Close(Type.Missing, Type.Missing, Type.Missing);
-                ExcelApp.Quit();
-                Marshal.ReleaseComObject(newSheet);
-                Marshal.ReleaseComObject(sheets);
-                Marshal.ReleaseComObject(workbook);
+            // workbook.ActiveSheet(@"c:\\temp\\test\" + vs.Name + ".csv");
+            //   newSheet.Name = vs.Name;
+            //   workbook.Save();
+            ExcelApp.Quit();
+            Marshal.ReleaseComObject(newSheet);
+            Marshal.ReleaseComObject(sheets);
+            Marshal.ReleaseComObject(workbook);
 
-                return r;
-           
+            return r;
         }
-    } 
+        //workbook.SaveAs(@"c:\\temp\\DIKKETEST", ".xlsx");
+        //workbook.Close(Type.Missing, Type.Missing, Type.Missing);
+    }
 }

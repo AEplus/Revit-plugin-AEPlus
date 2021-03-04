e259797eee7359e5cfb089b8379f8514fe708d5e
sbellinkx <39960806+sbellinkx@users.noreply.github.com>
2021-03-04 12:29:02 +0100

    NoErrors, not yet to one excel file

--- CSVPerTabToevoegen.cs
+++ CSVPerTabToevoegen.cs
@@ -2,8 +2,6 @@
 using Autodesk.Revit.DB;
 using Autodesk.Revit.UI;
 using System;
-using xlApp = Microsoft.Office.Interop.Excel;
-using System.Runtime.InteropServices;
 using OfficeOpenXml;
 using System.IO;
 
@@ -13,12 +11,9 @@ namespace MyRevitCommands
     public class CSVPerTabToevoegen : IExternalCommand
     {
         public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
-        {         
+        {
             Result r = Result.Succeeded;
-            xlApp.Application ExcelApp = null;
-            xlApp.Workbook workbook = null;
-            xlApp.Sheets sheets = null;
-            xlApp.Worksheet newSheet = null;
+            ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
 
             // MisValue for excel files, unknown values
             object misValue = System.Reflection.Missing.Value;
@@ -35,21 +30,20 @@ namespace MyRevitCommands
            Export Options on how the .txt file will look. Text "" is gone
            FieldDelimiter is TAB replaced with ,  
             */
-            ViewScheduleExportOptions opt = new ViewScheduleExportOptions();
-            //{
-            //    TextQualifier = ExportTextQualifier.None,
-            //    FieldDelimiter = ","
-            //};
-
-            
+            ViewScheduleExportOptions opt = new ViewScheduleExportOptions()
+            {
+                TextQualifier = ExportTextQualifier.DoubleQuote
+            };
 
             foreach (ViewSchedule vs in col)
             {
-                if (vs.Name.Contains("AE")){
+                if (vs.Name.Contains("AE"))
+                {
                     vs.Export(@"c:\\temp\", Environment.UserName + vs.Name + ".csv", opt);
 
 
                     FileInfo file = new FileInfo(@"c:\\temp\" + Environment.UserName + vs.Name + ".csv");
+
                     //create a new Excel package
                     using (ExcelPackage excel = new ExcelPackage())
                     {
@@ -63,57 +57,24 @@ namespace MyRevitCommands
 
                         //the path of the file
                         string filePath = "C:\\temp\\ExcelDemo.xlsx";
+
                         //Write the file to the disk
                         FileInfo fi = new FileInfo(filePath);
                         excel.SaveAs(fi);
                     }
                 }
-
-                // read the CSV file from disk
-                // FileInfo file = new FileInfo(@"c:\\temp\\test" + Environment.UserName + vs.Name + ".csv");
-
-                // var newFile = (@"c:\\temp\\test" + Environment.UserName + vs.Name + ".csv");
-
-
-                // DIT IS EPPlus package 
-                //using (ExcelPackage excelPackage = new ExcelPackage())
-                //{
-                //    //create a WorkSheet
-                //    ExcelWorksheet worksheet = excelPackage.Workbook.Worksheets.Add(vs.Name);
-
-                //    //load the CSV data into cell A1
-                //    worksheet.Cells["A1"].LoadFromText(file, format);
-
-                //    // file name with .xlsx extension  
-                //    string p_strPath = @"c:\\temp\\test.xlsx";
-
-                //    if (File.Exists(p_strPath))
-                //        File.Delete(p_strPath);
-
-                //    // Create excel file on physical disk  
-                //    FileStream objFileStrm = File.Create(p_strPath);
-                //    objFileStrm.Close();
-
-                //    // Write content to excel file  
-                //    File.WriteAllBytes(p_strPath, excelPackage.GetAsByteArray());
-                //    //Close Excel package 
-                //    excelPackage.Dispose();
-                // TOT HIER IS HET EPPlus Package
-
             }
             //newSheet = (Worksheet)sheets.Add(sheets[i], Type.Missing, Type.Missing, Type.Missing);
 
             // workbook.ActiveSheet(@"c:\\temp\\test\" + vs.Name + ".csv");
             //   newSheet.Name = vs.Name;
             //   workbook.Save();
-            ExcelApp.Quit();
-            Marshal.ReleaseComObject(newSheet);
-            Marshal.ReleaseComObject(sheets);
-            Marshal.ReleaseComObject(workbook);
+
+            // Marshal.ReleaseComObject(newSheet);
+            // Marshal.ReleaseComObject(sheets);
+            // Marshal.ReleaseComObject(workbook);
 
             return r;
         }
-        //workbook.SaveAs(@"c:\\temp\\DIKKETEST", ".xlsx");
-        //workbook.Close(Type.Missing, Type.Missing, Type.Missing);
     }
 }

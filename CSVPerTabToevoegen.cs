0ab18dce5abbc9bbdcae65ea98a4bcb9a56c54c7
sbellinkx <39960806+sbellinkx@users.noreply.github.com>
2021-03-04 16:12:45 +0100

    16u commit, blijft zelfde tab overschrijven

--- CSVPerTabToevoegen.cs
+++ CSVPerTabToevoegen.cs
@@ -5,6 +5,7 @@ using System;
 using OfficeOpenXml;
 using System.IO;
 using System.Linq;
+using System.Globalization;
 
 namespace MyRevitCommands
 {
@@ -33,27 +34,45 @@ namespace MyRevitCommands
             */
             ViewScheduleExportOptions opt = new ViewScheduleExportOptions()
             {
-                TextQualifier = ExportTextQualifier.DoubleQuote
+                TextQualifier = ExportTextQualifier.DoubleQuote,
+                FieldDelimiter = ","
             };
 
-            int i = 0;
+            int i = 1;
             foreach (ViewSchedule vs in col)
             {
+                using (ExcelPackage excelEngine = new ExcelPackage())
+                {
                 if (vs.Name.Contains("AE"))
                 {
                     vs.Export(@"c:\\temp\", Environment.UserName + vs.Name + ".csv", opt);
 
-                    FileInfo file = new FileInfo(@"c:\\temp\" + Environment.UserName + vs.Name + ".csv");
+                        FileInfo file = new FileInfo(@"c:\\temp\" + Environment.UserName + vs.Name + ".csv");
 
-                    using (ExcelPackage excelEngine = new ExcelPackage())
-                    {
-                        var xlWorkbook = excelEngine.Workbook.Worksheets.Add(vs.Name);
-                        xlWorkbook.Workbook.Worksheets.Count();
-                        xlWorkbook.Workbook.Worksheets.MoveToEnd(i);
-                        i++;
+                        //create a WorkSheet
+                        ExcelWorksheet ws = excelEngine.Workbook.Worksheets.Add(vs.Name);
+                        int totalsheets = excelEngine.Workbook.Worksheets.Count;
+
+                        var format = new ExcelTextFormat()
+                        {   
+                          Culture = CultureInfo.InvariantCulture,
+                        //    // Escape character for values containing the Delimiter
+                        //    // ex: "A,Name",1 --> two cells, not three
+                        TextQualifier = '"'
+                        //    // Other properties
+                        //    // EOL, DataTypes, Encoding, SkipLinesBeginning/End
+                        };
+                        ws.Cells["A1"].LoadFromText(file, format);
+                        
+                       // excelEngine.Workbook.Worksheets.MoveBefore(i);
+
+                        //the path of the file
+                        string filePath = "C:\\temp\\ExcelDemo.xlsx";
+
+                        //Write the file to the disk
+                        FileInfo fi = new FileInfo(filePath);
+                        excelEngine.SaveAs(fi);
                     }
-
-
                 }
             }
             return r;

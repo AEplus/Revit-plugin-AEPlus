fa47cc3dd2c5074c3957c705240b970cfa89bbab
sbellinkx <39960806+sbellinkx@users.noreply.github.com>
2021-03-05 08:57:24 +0100

    CreateFolderAddIn

--- CSVPerTabToevoegen.cs
+++ CSVPerTabToevoegen.cs
@@ -14,6 +14,8 @@ namespace MyRevitCommands
     {
         public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
         {
+            System.IO.Directory.CreateDirectory(@"c:\\temp\");
+
             Result r = Result.Succeeded;
             ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
 
@@ -27,11 +29,9 @@ namespace MyRevitCommands
 
             FilteredElementCollector col = new FilteredElementCollector(doc)
             .OfClass(typeof(ViewSchedule));
-
-            /*
-           Export Options on how the .txt file will look. Text "" is gone
-           FieldDelimiter is TAB replaced with ,  
-            */
+            
+           // Export Options on how the .txt file will look. Text "" is gone
+           // FieldDelimiter is TAB replaced with ,            
             ViewScheduleExportOptions opt = new ViewScheduleExportOptions()
             {
                 TextQualifier = ExportTextQualifier.DoubleQuote,
@@ -40,17 +40,16 @@ namespace MyRevitCommands
 
             using (ExcelPackage excelEngine = new ExcelPackage())
             {
-                
-            foreach (ViewSchedule vs in col)
-            {
+                foreach (ViewSchedule vs in col)
+                {
                     //create a WorkSheet
                     ExcelWorksheet ws = excelEngine.Workbook.Worksheets.Add(vs.Name);
 
                     // Searches for schedules containing AE 
                     if (vs.Name.Contains("AE"))
-                {
+                    {
                         // Export c:\\temp --> Will be save as
-                    vs.Export(@"c:\\temp\", Environment.UserName + vs.Name + ".csv", opt);
+                        vs.Export(@"c:\\temp\", Environment.UserName + vs.Name + ".csv", opt);
                         FileInfo file = new FileInfo(@"c:\\temp\" + Environment.UserName + vs.Name + ".csv");
 
                         // Adds Worksheet as first in the row 
@@ -58,25 +57,25 @@ namespace MyRevitCommands
 
                         // Formating for writing to xlsx
                         var format = new ExcelTextFormat()
-                        {   
-                          Culture = CultureInfo.InvariantCulture,
-                        //    // Escape character for values containing the Delimiter
-                        //    // ex: "A,Name",1 --> two cells, not three
-                        TextQualifier = '"'
-                        //    // Other properties
-                        //    // EOL, DataTypes, Encoding, SkipLinesBeginning/End
+                        {
+                            Culture = CultureInfo.InvariantCulture,
+                            //    // Escape character for values containing the Delimiter
+                            //    // ex: "A,Name",1 --> two cells, not three
+                            TextQualifier = '"'
+                            //    // Other properties
+                            //    // EOL, DataTypes, Encoding, SkipLinesBeginning/End
                         };
                         ws.Cells["A1"].LoadFromText(file, format);
 
                         // excelEngine.Workbook.Worksheets.MoveBefore(i);
                         //the path of the file
-                        string filePath = "C:\\temp\\ExcelDemo.xlsx";
+                        string filePath = "C:\\temp\\ExcelSchedulesTotaal.xlsx";
 
                         //Write the file to the disk
                         FileInfo fi = new FileInfo(filePath);
                         excelEngine.SaveAs(fi);
                     }
-                }  
+                }
             }
             return r;
         }

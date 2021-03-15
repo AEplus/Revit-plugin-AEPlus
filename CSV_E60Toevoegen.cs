217010d3d30c7978ef28993c58024f7c67b90c38
Sebastiaan Bellinkx <sebastiaan.bellinkx@protonmail.com>
2021-03-15 14:01:22 +0100

    Met update voor filteren van .csv files. Joren Jamar thanks hiervoor

--- CSV_E60Toevoegen.cs
+++ CSV_E60Toevoegen.cs
@@ -5,9 +5,8 @@ using System;
 using OfficeOpenXml;
 using System.IO;
 using System.Globalization;
+using CsvHelper;
 using Excel = Microsoft.Office.Interop.Excel;
-using DocumentFormat.OpenXml.Packaging;
-using DocumentFormat.OpenXml.Spreadsheet;
 using System.Linq;
 using System.Collections.Generic;
 using System.Diagnostics;
@@ -23,9 +22,7 @@ namespace E_60Toevoegen
             Result r = Result.Succeeded;
             ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
 
-            // MisValue for excel files, unknown values
-            object misValue = System.Reflection.Missing.Value;
-            int j = 0;
+            string emptyFirstCellDocument = "";
 
             // Get UIDocument
             UIDocument uidoc = commandData.Application.ActiveUIDocument;
@@ -43,9 +40,21 @@ namespace E_60Toevoegen
                 FieldDelimiter = ","
             };
 
+            // Formating for writing to xlsx
+            var format = new ExcelTextFormat()
+            {
+                Culture = CultureInfo.InvariantCulture,
+                // Escape character for values containing the Delimiter
+                // ex: "A,Name",1 --> two cells, not three
+                TextQualifier = '"'
+                // Other properties
+                // EOL, DataTypes, Encoding, SkipLinesBeginning/End
+            };
+
             // Creates new excelpackage this 
             using (ExcelPackage excelEngine = new ExcelPackage())
             {
+                ExcelWorksheet wbUitzondering = excelEngine.Workbook.Worksheets.Add("Uitzondering");
                 foreach (ViewSchedule vs in col)
                 {
                     // Searches for schedules containing AE E60 M52 en M57 ventilatierooster
@@ -59,26 +68,41 @@ namespace E_60Toevoegen
                         //create a WorkSheet
                         ExcelWorksheet ws1 = excelEngine.Workbook.Worksheets.Add(vs.Name);
                         // Export c:\\temp --> Will be save as
-                        vs.Export(@"c:\\temp\\E_60\", Environment.UserName + vs.Name + ".csv", opt);
-                        FileInfo file = new FileInfo(@"c:\\temp\\E_60\" + Environment.UserName + vs.Name + ".csv");
+                        string filename = Environment.UserName + vs.Name;
 
+                        vs.Export(@"c:\\temp\\E_60\", filename + ".csv", opt);               
+
+                        // Lege eerste kolom eruithalen. Dan zo 1 grote .csv met alle "lege" in en dan nog altijd per .csv per sheet
+                        string normalDocument = "";
+                        foreach (string line in File.ReadAllLines(@"c:\\temp\\E_60\" + filename + ".csv"))
+                        {    
+                            if (line.Split(',')[0] == "")
+                            {
+                                emptyFirstCellDocument += line + Environment.NewLine;
+                            }
+                            else
+                            {
+                                normalDocument += line + Environment.NewLine;
+                            }
+                        }
+
+                        File.WriteAllText(@"c:\\temp\\E_60\" + filename + ".csv", normalDocument);
+                        FileInfo file = new FileInfo(@"c:\\temp\\E_60\" + filename + ".csv");
                         // Adds Worksheet as first in the row 
                         ws1.Workbook.Worksheets.MoveToStart(vs.Name);
 
                         // Formating for writing to xlsx
-                        var format = new ExcelTextFormat()
-                        {
-                            Culture = CultureInfo.InvariantCulture,
-                            // Escape character for values containing the Delimiter
-                            // ex: "A,Name",1 --> two cells, not three
-                            TextQualifier = '"'
-                            // Other properties
-                            // EOL, DataTypes, Encoding, SkipLinesBeginning/End
-                        };
-
+                        //var format = new ExcelTextFormat()
+                        //{
+                        //    Culture = CultureInfo.InvariantCulture,
+                        //    // Escape character for values containing the Delimiter
+                        //    // ex: "A,Name",1 --> two cells, not three
+                        //    TextQualifier = '"'
+                        //    // Other properties
+                        //    // EOL, DataTypes, Encoding, SkipLinesBeginning/End
+                        //};
                         ws1.Cells["A1"].LoadFromText(file, format);
 
-                        // excelEngine.Workbook.Worksheets.MoveBefore(i);
                         // the path of the file
                         string filePath = "C:\\temp\\E_60\\Excel_E_60.xlsx";
 
@@ -86,46 +110,21 @@ namespace E_60Toevoegen
                         FileInfo fi = new FileInfo(filePath);
                         excelEngine.SaveAs(fi);
                     }
-                }
-                excelEngine.Dispose();
-            } 
-            
-            // om vervolgens de rij te kopieren. 
-            //Create COM Objects. Create a COM object for everything that is referenced
-            Excel.Application xlApp = new Excel.Application();
-            Excel.Workbook xlWorkbook = xlApp.Workbooks.Open(@"C:\\temp\\E_60\\Excel_E_60.xlsx");
-            // Excel.Workbook wb2 = xlApp.Workbooks.Open("c:\\temp\\E_60\\Missing.xlsx");
+                }            
 
-            foreach (Excel.Worksheet xlworksheet in xlWorkbook.Worksheets)
-            {
-                // select worksheet. NOT zero-based!!:
-                Excel._Worksheet excelWorkbookWorksheet = xlWorkbook.Sheets[1];
-                // create a range:
-                Excel.Range usedRange = excelWorkbookWorksheet.UsedRange;
-                // iterate range
-                foreach (Excel.Range range1 in usedRange)
-                {
-                    // check condition:
-                    if (range1.Value2 == "")
-                        j++;
-                    // if match, cut and shift remaining cells up:
-                    range1.Delete(Excel.XlDeleteShiftDirection.xlShiftUp);
-                }
-                // save changes (!!):
-                xlWorkbook.Save();
-                // cleanup:
-                if (xlApp != null)
-                {
-                    Process[] pProcess;
-                    pProcess = System.Diagnostics.Process.GetProcessesByName("Excel");
-                    pProcess[0].Kill();
-                }
+                File.WriteAllText(@"c:\\temp\\E_60\Uitzonderingen.csv", emptyFirstCellDocument);
+                FileInfo fileUitzondering = new FileInfo(@"c:\\temp\\E_60\Uitzonderingen.csv");
+                wbUitzondering.Cells["A1"].LoadFromText(fileUitzondering, format);
+
+                string stringPath = "C:\\temp\\E_60\\Uitzonderingen.xlsx";
+
+                // Write the file to the disk
+                FileInfo fileInfoUitzondering = new FileInfo(stringPath);
+                excelEngine.SaveAs(fileInfoUitzondering);
+
+                excelEngine.Dispose();
             }
-            // result succeeded 
             return r;
         }
     }
 }
-
-
-

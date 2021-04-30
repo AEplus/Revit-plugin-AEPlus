674adc242fcfe4492e5e4648c90080337dc53ce7
Sebastiaan Bellinkx <sebastiaan.bellinkx@protonmail.com>
2021-04-30 09:11:53 +0200

    FieldDelimiter changed to double quote

--- SanAantal.cs
+++ SanAantal.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using Autodesk.Revit.Attributes;
 using Autodesk.Revit.DB;
 using Autodesk.Revit.UI;
@@ -10,14 +11,23 @@ namespace MyRevitCommands
     {
         public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
         {
-            var revitSchedules = new ArrayList();
-            revitSchedules.Add("AE_M56");
-            revitSchedules.Add("ventilatierooster");
+            try
+            {
+                var revitSchedules = new ArrayList();
+                // revitSchedules.Add("AE_M56");
+                // revitSchedules.Add("ventilatierooster");
+                revitSchedules.Add("ZAE");
 
-            var fileName = GetType().Name;
+                var fileName = GetType().Name;
 
-            return new GenericToevoegen().GenericExecute(commandData, ref message, elements, @"c:\\temp\\Sanitair\",
-                revitSchedules, fileName);
+                return new GenericToevoegen().GenericExecute(commandData, ref message, elements, @"c:\\temp\\Sanitair\",
+                    revitSchedules, fileName);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
         }
     }
 }

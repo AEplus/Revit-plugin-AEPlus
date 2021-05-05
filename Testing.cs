46c6eb0bcc91983eeac3f408f8c878e3a004f96b
Sebastiaan Bellinkx <sebastiaan.bellinkx@protonmail.com>
2021-05-05 08:45:26 +0200

    testing button added

---
+++ Testing.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+
+namespace MyRevitCommands
+{
+    [TransactionAttribute(TransactionMode.ReadOnly)]
+    public class Testing : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            var revitSchedules = new ArrayList();
+            revitSchedules.Add("ZAE_M50_Leidingen-Zaag isolatie SAN L");
+
+            var fileName = GetType().Name;
+
+            return new GenericToevoegen().GenericExecute(commandData, ref message, elements, @"c:\\temp\\Sanitair\",
+                revitSchedules, fileName);
+        }
+    }
+}

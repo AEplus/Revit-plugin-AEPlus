3beadb4569e0a06645608f27a4ce0bd1c38bc237
Sebastiaan Bellinkx <sebastiaan.bellinkx@protonmail.com>
2021-05-05 11:31:16 +0200

    Export werkt. Empty and full should work aswell. Isolation problem is fixed and returns emprty now.

--- GenericToevoegen.cs
+++ GenericToevoegen.cs
@@ -1,50 +1,47 @@
-﻿using Autodesk.Revit.DB;
-using Autodesk.Revit.UI;
-using OfficeOpenXml;
-using System;
+﻿using System;
 using System.Collections;
-using System.Collections.Generic;
 using System.Globalization;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using OfficeOpenXml;
 
 namespace MyRevitCommands
 {
-    class GenericToevoegen
+    internal class GenericToevoegen
     {
         /*
             ...
         MapPath example value:  @"c:\\temp\\E_60\" 
          */
-        public Result GenericExecute(ExternalCommandData commandData, ref string message, ElementSet elements, string MapPath, ArrayList revitSchedules, string fileName)
+        public Result GenericExecute(ExternalCommandData commandData, ref string message, ElementSet elements,
+            string MapPath, ArrayList revitSchedules, string fileName)
         {
             string xlSheetName;
-            System.IO.Directory.CreateDirectory(MapPath);
-            Result r = Result.Succeeded;
+            Directory.CreateDirectory(MapPath);
+            var r = Result.Succeeded;
             ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
-            string emptyFirstCellDocument = "";
-            string TotalDocument = "";
+            var emptyFirstCellDocument = "";
+            var TotalDocument = "";
 
             // Get UIDocument
-            UIDocument uidoc = commandData.Application.ActiveUIDocument;
+            var uidoc = commandData.Application.ActiveUIDocument;
             // Get Document
-            Document doc = uidoc.Document;
+            var doc = uidoc.Document;
 
-            FilteredElementCollector col = new FilteredElementCollector(doc)
-            .OfClass(typeof(ViewSchedule));
+            var col = new FilteredElementCollector(doc)
+                .OfClass(typeof(ViewSchedule));
 
             // Export Options on how the .txt file will look. Text "" is gone
             // FieldDelimiter is TAB replaced with ,
-            ViewScheduleExportOptions opt = new ViewScheduleExportOptions()
+            var opt = new ViewScheduleExportOptions
             {
                 TextQualifier = ExportTextQualifier.None,
-                FieldDelimiter = ",",
+                FieldDelimiter = ","
             };
 
             // Formating for writing to xlsx
-            var format = new ExcelTextFormat()
+            var format = new ExcelTextFormat
             {
                 Culture = CultureInfo.InvariantCulture,
                 // Escape character for values containing the Delimiter
@@ -54,43 +51,38 @@ namespace MyRevitCommands
                 // EOL, DataTypes, Encoding, SkipLinesBeginning/End
             };
             // Creates new excelpackage this
-            using (ExcelPackage excelEngine = new ExcelPackage())
+            using (var excelEngine = new ExcelPackage())
             {
-                using (ExcelPackage xlPackage = new ExcelPackage())
+                using (var xlPackage = new ExcelPackage())
                 {
-                    ExcelWorksheet wbUitzondering = xlPackage.Workbook.Worksheets.Add("Uitzondering");
-                    ExcelWorksheet wbAllesIngevuld = xlPackage.Workbook.Worksheets.Add("AllesIngevuld");
+                    var wbUitzondering = xlPackage.Workbook.Worksheets.Add("Uitzondering");
+                    var wbAllesIngevuld = xlPackage.Workbook.Worksheets.Add("AllesIngevuld");
                     foreach (ViewSchedule vs in col)
-                    {
                         // Searches for schedules containing AE E60 M52 en M57 ventilatierooster
                         // dit zijn de schedules waarbij het met aantallen is.
 
-                        if(checkValues(vs.Name, revitSchedules))
+                        if (checkValues(vs.Name, revitSchedules))
                         {
                             // Checks if vs.name is not longer than 30 characters because Excels crashes.
                             // Keep in mind for the vs.names that they have to be different under 30 characters
                             // Excel does not like the same names
                             if (vs.Name.Length > 30)
-                            {
                                 xlSheetName = vs.Name.Substring(0, 30);
-                            }
                             else
-                            {
                                 xlSheetName = vs.Name;
-                            }
                             //create a WorkSheet
-                            ExcelWorksheet ws1 = excelEngine.Workbook.Worksheets.Add(xlSheetName);
+                            var ws1 = excelEngine.Workbook.Worksheets.Add(xlSheetName);
                             // Export c:\\temp --> Will be save as
-                            string filename = Environment.UserName + vs.Name;
+                            var filename = Environment.UserName + vs.Name;
                             vs.Export(MapPath, filename + ".csv", opt);
 
-                            string normalDocument = "";
-                            string StringPathFile = MapPath + filename + ".csv";
-                            string[] lines = File.ReadAllLines(StringPathFile);
-                            char[] delimitChars = { ',' };
-                            int i = 1;
+                            var normalDocument = "";
+                            var StringPathFile = MapPath + filename + ".csv";
+                            var lines = File.ReadAllLines(StringPathFile);
+                            char[] delimitChars = {','};
+                            var i = 1;
 
-                            foreach (string line in lines)
+                            foreach (var line in lines)
                             {
                                 // Gets first 2 row of each Schedule, name and properties.
                                 // This is done for visibility and making the excel easier to read
@@ -103,50 +95,46 @@ namespace MyRevitCommands
                                 // Looks for first value if this is null or blank ""
                                 // Geo-IT number has to be first for this line
                                 // Checks if it is blank or not
-                                if (line.Split(delimitChars)[0] == ""
-                                    || line.Split(delimitChars)[0] == null)
-                                {
+                                if (line.Split(delimitChars)[0] == "")
                                     // Empty first cell .csv line
                                     emptyFirstCellDocument += line + Environment.NewLine;
-                                }
                                 else
-                                {
                                     // If the first cell is filled, continue on this .csv line
                                     // Only checks for blanks or not!
                                     normalDocument += line + Environment.NewLine;
-                                }
                             }
+
                             // Gets spacing for each schedule.
                             // Increasing visibility for reading the excel.
-                            emptyFirstCellDocument += Environment.NewLine + Environment.NewLine;
+                            emptyFirstCellDocument += Environment.NewLine;
                             TotalDocument += normalDocument + Environment.NewLine;
 
-                            File.WriteAllText(MapPath + filename + ".csv", normalDocument.ToString());
-                            FileInfo file = new FileInfo(MapPath + filename + ".csv");
+                            File.WriteAllText(MapPath + filename + ".csv", normalDocument);
+                            var file = new FileInfo(MapPath + filename + ".csv");
                             // Adds Worksheet as first in the row because excel keeps adding in the back
                             ws1.Workbook.Worksheets.MoveToStart(xlSheetName);
                             // EPPlus method for reading a .csv to excel in the format as written above here line +-40
                             ws1.Cells["A1"].LoadFromText(file, format);
                             // the path of the file
-                            string filePath = MapPath + fileName + ".xlsx";
+                            var filePath = MapPath + fileName + ".xlsx";
                             // Write the file to the disk
-                            FileInfo fi = new FileInfo(filePath);
+                            var fi = new FileInfo(filePath);
                             excelEngine.SaveAs(fi);
 
                             // Exporting .csv file when first cell is empty
                             File.WriteAllText(MapPath + "Uitzonderingen.csv", emptyFirstCellDocument);
-                            FileInfo fileUitzondering = new FileInfo(MapPath + "Uitzonderingen.csv");
+                            var fileUitzondering = new FileInfo(MapPath + "Uitzonderingen.csv");
                             // Load the empty.csv to the worksheet
                             wbUitzondering.Cells["A1"].LoadFromText(fileUitzondering, format);
-                            string stringPath = MapPath + fileName+ "Overzicht.xlsx";
+                            var stringPath = MapPath + fileName + "Overzicht.xlsx";
 
                             // When first cell is not blank, write to filled.csv
                             File.WriteAllText(MapPath + "TotaalIngevuld.csv", TotalDocument);
-                            FileInfo fileTotaalIngevuld = new FileInfo(MapPath + "TotaalIngevuld.csv");
+                            var fileTotaalIngevuld = new FileInfo(MapPath + "TotaalIngevuld.csv");
                             wbAllesIngevuld.Cells["A1"].LoadFromText(fileTotaalIngevuld, format);
 
                             // Write the file to the disk
-                            FileInfo fileInfoUitzondering = new FileInfo(stringPath);
+                            var fileInfoUitzondering = new FileInfo(stringPath);
                             xlPackage.SaveAs(fileInfoUitzondering);
 
                             // For each loop this deletes the .csv files to keep the folder clean
@@ -154,27 +142,25 @@ namespace MyRevitCommands
                             File.Delete(MapPath + "Uitzonderingen.csv");
                             File.Delete(MapPath + "TotaalIngevuld.csv");
                         }
-                        //FileInfo fileIngevuld = new FileInfo(stringpath);
-                        //xlPackage.SaveAs(fileIngevuld);
-                    }
+                    //FileInfo fileIngevuld = new FileInfo(stringpath);
+                    //xlPackage.SaveAs(fileIngevuld);
+
                     xlPackage.Dispose();
                 }
+
                 excelEngine.Dispose();
             }
+
             return r;
         }
-        private Boolean checkValues(string name, ArrayList toCheck)
+
+        private bool checkValues(string name, ArrayList toCheck)
         {
-            Boolean current = false;
+            var current = false;
             foreach (string item in toCheck)
-            {
                 if (name.Contains(item))
-                {
                     current = true;
-                }
-            }
             return current;
         }
-
     }
 }

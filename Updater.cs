98d327df0cca318c5daa0a6d074af412c6a97330
Sebastiaan Bellinkx <sebastiaan.bellinkx@protonmail.com>
2021-06-03 15:58:50 +0200

    Updater works with AE Opmeting

--- Updater.cs
+++ Updater.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using Autodesk.Revit.Attributes;
 using Autodesk.Revit.DB;
 using Autodesk.Revit.UI;
@@ -13,30 +14,82 @@ namespace MyRevitCommands
             var app = commandData.Application;
             var doc = app.ActiveUIDocument.Document;
 
-            var sortedElements
-                = new Dictionary<string, List<Element>>();
-
-            // Iterate over all elements, both symbols and 
-            // model elements, and them in the dictionary.
-
-            ElementFilter f = new LogicalOrFilter(
-                new ElementIsElementTypeFilter(false),
-                new ElementIsElementTypeFilter(true));
-
-            var collector
-                = new FilteredElementCollector(doc)
-                    .WherePasses(f);
-
-            string name;
-
-            foreach (var e in collector)
+            using (var transaction = new Transaction(doc))
             {
-                var parameter = e.LookupParameter("AE Opmeting");
-                // var Opmeting = parameter.AsInteger();
+                if (transaction.Start("Replace null with zero") == TransactionStatus.Started)
+                {
+                    BuiltInCategory[] bics =
+                    {
+                        BuiltInCategory.OST_CableTray,
+                        BuiltInCategory.OST_CableTrayFitting,
+                        BuiltInCategory.OST_Conduit,
+                        BuiltInCategory.OST_ConduitFitting,
+                        BuiltInCategory.OST_DuctCurves,
+                        BuiltInCategory.OST_DuctFitting,
+                        BuiltInCategory.OST_DuctTerminal,
+                        BuiltInCategory.OST_ElectricalEquipment,
+                        BuiltInCategory.OST_ElectricalFixtures,
+                        BuiltInCategory.OST_LightingDevices,
+                        BuiltInCategory.OST_LightingFixtures,
+                        BuiltInCategory.OST_MechanicalEquipment,
+                        BuiltInCategory.OST_PipeCurves,
+                        BuiltInCategory.OST_PipeFitting,
+                        BuiltInCategory.OST_PlumbingFixtures,
+                        BuiltInCategory.OST_SpecialityEquipment,
+                        BuiltInCategory.OST_Sprinklers,
+                        BuiltInCategory.OST_Wire
+                    };
 
-                if (null != parameter)
-                    if (string.IsNullOrEmpty(parameter.AsValueString()))
-                        parameter.Set(0);
+                    IList<ElementFilter> a
+                        = new List<ElementFilter>(bics.Count());
+
+                    foreach (var bic in bics) a.Add(new ElementCategoryFilter(bic));
+                    var categoryFilter
+                        = new LogicalOrFilter(a);
+
+                    ElementFilter f = new LogicalAndFilter(
+                        categoryFilter,
+                        new ElementIsElementTypeFilter(true));
+
+                    var collector
+                        = new FilteredElementCollector(doc)
+                            .WherePasses(f);
+
+                    ElementId id = null;
+
+                    foreach (var e in collector)
+                    {
+                        var parameter = e.LookupParameter("AE Opmeting");
+
+                        if (parameter.AsDouble() == 0) parameter.Set(0);
+                        // var P = doc.GetElement(e.Id).LookupParameter("AE Opmeting");
+                        // if (P != null || P.AsValueString() != "0" || P.AsValueString() != null) P.Set("0");
+                    }
+                }
+
+                transaction.Commit();
+
+
+                // Above now auto commits. 
+                //// Ask the end user whether the changes are to be committed or not
+                //var taskDialog = new TaskDialog("Revit");
+                //taskDialog.MainContent = "Click either [OK] to Commit, or [Cancel] to Roll back the transaction.";
+                //var buttons = TaskDialogCommonButtons.Ok | TaskDialogCommonButtons.Cancel;
+                //taskDialog.CommonButtons = buttons;
+
+                //if (TaskDialogResult.Ok == taskDialog.Show())
+                //{
+                //    // For many various reasons, a transaction may not be committed
+                //    // if the changes made during the transaction do not result a valid model.
+                //    // If committing a transaction fails or is canceled by the end user,
+                //    // the resulting status would be RolledBack instead of Committed.
+                //    if (TransactionStatus.Committed != transaction.Commit())
+                //        TaskDialog.Show("Failure", "Transaction could not be committed");
+                //}
+                //else
+                //{
+                //    transaction.RollBack();
+                //}
             }
 
             return Result.Succeeded;

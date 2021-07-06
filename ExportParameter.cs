6a8c85da2e6395920f0bdc00752b280c8f5e3bad
Sebastiaan Bellinkx <sebastiaan.bellinkx@protonmail.com>
2021-07-06 21:32:44 +0200

    Import werkt, export heeft nog niet alles

--- ExportParameter.cs
+++ ExportParameter.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using System.Linq;
 using Autodesk.Revit.Attributes;
 using Autodesk.Revit.DB;
 using Autodesk.Revit.UI;
@@ -41,14 +42,14 @@ namespace MyRevitCommands
                 wsParameters.Cells[1, 3].Value = "GeoIT Value";
                 wsParameters.Cells[1, 4].Value = "Unique ID";
 
-                var families
+                var fs
                     = new FilteredElementCollector(doc)
-                        .OfClass(typeof(FamilySymbol));
+                        .OfClass(typeof(FamilySymbol)).Cast<FamilySymbol>().ToList();
 
-                var fs = families.WhereElementIsElementType().ToElements();
+                // var fs = families.WhereElementIsElementType().ToElements();
                 var rij = 2;
 
-                foreach (FamilySymbol e in fs)
+                foreach (var e in fs)
                 {
                     wsParameters.Cells[rij, 1].Value = e.FamilyName;
                     wsParameters.Cells[rij, 2].Value = e.Name;
--- ImportParameter.cs
+++ ImportParameter.cs
@@ -1,4 +1,5 @@
-﻿using System.Diagnostics;
+﻿using System;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using Autodesk.Revit.Attributes;
@@ -12,6 +13,9 @@ namespace MyRevitCommands
     [Transaction(TransactionMode.Manual)]
     public class ImportParameter : IExternalCommand
     {
+        private Guid guid;
+        private ExcelWorksheet worksheet;
+
         public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
         {
             var uiapp = commandData.Application;
@@ -23,7 +27,7 @@ namespace MyRevitCommands
             var group = file.Groups.get_Item("00. General");
             var definition = group.Definitions.get_Item("AE GeoIT Link");
             var externalDefinition = definition as ExternalDefinition;
-            var guid = externalDefinition.GUID;
+            guid = externalDefinition.GUID;
 
             var dlg = new WinForms.OpenFileDialog();
             dlg.Title = "Select source Excel file from which to update Revit shared parameters";
@@ -34,7 +38,7 @@ namespace MyRevitCommands
 
             using (var excelEngine = new ExcelPackage(filename))
             {
-                var worksheet = excelEngine.Workbook.Worksheets[1];
+                worksheet = excelEngine.Workbook.Worksheets[1];
                 var iRowCnt = worksheet.Dimension.End.Row;
 
                 using (var t = new Transaction(doc))
@@ -49,43 +53,39 @@ namespace MyRevitCommands
                     //var families = fs.WhereElementIsElementType().ToElements();
 
                     foreach (var type in fs)
-                    {
-                        BeginLoop:
-                        if (!type.IsActive) type.Activate();
-
-                        if (row >= iRowCnt) row = 2;
-
-                        var famname = type.FamilyName;
-                        var famsymbol = type.Name;
-                        var excelfamily = (string) worksheet.Cells[row, 1].Value;
-                        var excelfs = (string) worksheet.Cells[row, 2].Value;
-                        var gvalue = (double) worksheet.Cells[row, 3].Value;
-
-                        if (famname == excelfamily)
-                            if (famsymbol == excelfs)
-                            {
-                                var fspara = type.get_Parameter(guid);
-                                if (fspara == null) continue;
-
-                                if (fspara.IsReadOnly != true)
+                        /*
+                            BeginLoop:
+                           // if (!type.IsActive) type.Activate();
+    
+                           
+                            if (row >= iRowCnt) row = 2;
+    
+                            var famname = type.FamilyName;
+                            var famsymbol = type.Name;
+                            var excelfamily = (string) worksheet.Cells[row, 1].Value;
+                            var excelfs = (string) worksheet.Cells[row, 2].Value;
+                            var gvalue = (double) worksheet.Cells[row, 3].Value;
+    
+                            if (famname.Equals(excelfamily))
+                                if (famsymbol.Equals(excelfs))
                                 {
-                                    fspara.Set(gvalue);
-                                    Debug.WriteLine(gvalue + famname + "  " + famsymbol);
+                                    var fspara = type.get_Parameter(guid);
+                                    if (fspara == null) continue;
+    
+                                    if (fspara.IsReadOnly != true)
+                                    {
+                                        fspara.Set(gvalue);
+                                        Debug.WriteLine(gvalue + famname + "  " + famsymbol);
+                                    }
                                 }
+    
+                            if (famname != excelfamily || famsymbol != excelfs)
+                            {
+                                row++;
+                                goto BeginLoop;
                             }
-
-                        if (famname != excelfamily)
-                        {
-                            row++;
-                            goto BeginLoop;
-                        }
-
-                        if (famsymbol != excelfs)
-                        {
-                            row++;
-                            goto BeginLoop;
-                        }
-                    }
+                           */
+                        checkFamily(row, iRowCnt, type);
 
                     t.Commit();
                     return Result.Succeeded;
@@ -93,6 +93,43 @@ namespace MyRevitCommands
             }
 
             return Result.Succeeded;
+        }
+
+        private void checkFamily(int row, int iRowCnt, FamilySymbol type)
+        {
+            //if (row >= iRowCnt) row = 2;
+            //Debug.Print("KIJK: " + row +
+            //" !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+            string famname = null;
+            string famsymbol = null;
+            if (type != null)
+            {
+                famname = type.FamilyName;
+                famsymbol = type.Name;
+            }
+
+            var excelfamily = (string) worksheet.Cells[row, 1].Value;
+            var excelfs = (string) worksheet.Cells[row, 2].Value;
+            var gvalue = (double) worksheet.Cells[row, 3].Value;
+
+            var skip = false;
+            if (famname.Equals(excelfamily))
+                if (famsymbol.Equals(excelfs))
+                {
+                    var fspara = type.get_Parameter(guid);
+                    if (fspara == null)
+                    {
+                        skip = true;
+                    }
+                    else if (fspara.IsReadOnly != true)
+                    {
+                        fspara.Set(gvalue);
+                        Debug.WriteLine(gvalue + famname + "  " + famsymbol);
+                    }
+                }
+
+            if (!skip && row + 1 < iRowCnt && (famname != excelfamily || famsymbol != excelfs))
+                checkFamily(row + 1, iRowCnt, type);
         }
     }
 }

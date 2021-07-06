1c85e9a801dd160af95d5997bcb0aab9bf897ad1
Sebastiaan Bellinkx <sebastiaan.bellinkx@protonmail.com>
2021-07-06 17:25:13 +0200

    debug komt erdoor. Effectief schrijven nog niet.

--- ImportParameter.cs
+++ ImportParameter.cs
@@ -1,6 +1,6 @@
-﻿using System;
-using System.Diagnostics;
+﻿using System.Diagnostics;
 using System.IO;
+using System.Linq;
 using Autodesk.Revit.Attributes;
 using Autodesk.Revit.DB;
 using Autodesk.Revit.UI;
@@ -17,86 +17,82 @@ namespace MyRevitCommands
             var uiapp = commandData.Application;
             var app = uiapp.Application;
             var doc = uiapp.ActiveUIDocument.Document;
+            var row = 2;
 
-            var guid = Guid.Empty;
-            try
-            {
-                var file = app.OpenSharedParameterFile();
-                var group = file.Groups.get_Item("00. General");
-                var definition = group.Definitions.get_Item("AE GeoIT Link");
-                var externalDefinition = definition as ExternalDefinition;
-                guid = externalDefinition.GUID;
-                Console.WriteLine(guid);
-            }
-            catch (Exception)
-            {
-            }
+            var file = app.OpenSharedParameterFile();
+            var group = file.Groups.get_Item("00. General");
+            var definition = group.Definitions.get_Item("AE GeoIT Link");
+            var externalDefinition = definition as ExternalDefinition;
+            var guid = externalDefinition.GUID;
 
             var dlg = new WinForms.OpenFileDialog();
             dlg.Title = "Select source Excel file from which to update Revit shared parameters";
             dlg.Filter = "Excel spreadsheet files (*.xls;*.xlsx)|*.xls;*.xlsx|All files (*)|*";
             if (WinForms.DialogResult.OK != dlg.ShowDialog()) return Result.Failed;
-
-
             var filename = new FileInfo(dlg.FileName);
-            var filepath = Path.GetDirectoryName(dlg.FileName);
 
 
             using (var excelEngine = new ExcelPackage(filename))
             {
-                var workbook = excelEngine.Workbook;
                 var worksheet = excelEngine.Workbook.Worksheets[1];
+                var iRowCnt = worksheet.Dimension.End.Row;
 
                 using (var t = new Transaction(doc))
                 {
-                    if (t.Start("Shared parameter values invullen") == TransactionStatus.Started)
+                    t.Start("started");
+                    //if (t.Start("Shared parameter values invullen") == TransactionStatus.Started)
+                    //{
+                    var fs
+                        = new FilteredElementCollector(doc)
+                            .OfClass(typeof(FamilySymbol)).Cast<FamilySymbol>().ToList();
+
+                    //var families = fs.WhereElementIsElementType().ToElements();
+
+                    foreach (var type in fs)
                     {
-                        var families
-                            = new FilteredElementCollector(doc)
-                                .OfClass(typeof(FamilySymbol));
+                        BeginLoop:
+                        if (!type.IsActive) type.Activate();
 
-                        var fs = families.WhereElementIsElementType().ToElements();
+                        if (row >= iRowCnt) row = 2;
 
-                        foreach (FamilySymbol type in fs)
+                        var famname = type.FamilyName;
+                        var famsymbol = type.Name;
+                        var excelfamily = (string) worksheet.Cells[row, 1].Value;
+                        var excelfs = (string) worksheet.Cells[row, 2].Value;
+                        var gvalue = (double) worksheet.Cells[row, 3].Value;
+
+                        if (famname == excelfamily)
+                            if (famsymbol == excelfs)
+                            {
+                                var fspara = type.get_Parameter(guid);
+                                if (fspara == null) continue;
+
+                                if (fspara.IsReadOnly != true)
+                                {
+                                    fspara.Set(gvalue);
+                                    Debug.WriteLine(gvalue + famname + "  " + famsymbol);
+                                }
+                            }
+
+                        if (famname != excelfamily)
                         {
-                            var rij = 2;
-
-                            try
-                            {
-                                var famname = type.FamilyName;
-                                var famsymbol = type.Name;
-                                var excelfamily = worksheet.Cells[rij, 1].Value;
-                                var excelfs = worksheet.Cells[rij, 2].Value;
-                                var gvalue = (double) worksheet.Cells[rij, 3].Value;
-
-
-                                if (Equals(famname, excelfamily))
-                                    if (Equals(famsymbol, excelfs))
-                                    {
-                                        var fspara = type.get_Parameter(guid);
-                                        fspara.Set(gvalue);
-                                        Debug.WriteLine(famname + famsymbol);
-                                        Debug.WriteLine(gvalue);
-                                    }
-
-
-                                Console.WriteLine("Volgende familysymbol");
-                            }
-
-                            catch (Exception exception)
-                            {
-                                Debug.WriteLine(exception);
-                            }
-
-                            rij++;
+                            row++;
+                            goto BeginLoop;
                         }
 
-                        t.Commit();
+                        if (famsymbol != excelfs)
+                        {
+                            row++;
+                            goto BeginLoop;
+                        }
                     }
-                }
 
-                return Result.Succeeded;
+                    t.Commit();
+                    return Result.Succeeded;
+                }
             }
+
+            return Result.Succeeded;
         }
     }
 }

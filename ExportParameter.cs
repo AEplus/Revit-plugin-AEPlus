1aecd71c3634c4ea4fdff6ecb6fdfada7dbeaec5
Sebastiaan Bellinkx <sebastiaan.bellinkx@protonmail.com>
2021-07-08 12:58:06 +0200

    Added comments

--- ExportParameter.cs
+++ ExportParameter.cs
@@ -5,7 +5,6 @@ using Autodesk.Revit.DB;
 using Autodesk.Revit.UI;
 using OfficeOpenXml;
 using WinForms = System.Windows.Forms;
-using X = Microsoft.Office.Interop.Excel;
 
 namespace MyRevitCommands
 {
@@ -21,12 +20,14 @@ namespace MyRevitCommands
             var guid = Guid.Empty;
             try
             {
+                // Gets GUID of AE GeoIT Link parameter. 
+                // This is done for because name could cause errors. 
                 var file = app.OpenSharedParameterFile();
                 var group = file.Groups.get_Item("00. General");
                 var definition = group.Definitions.get_Item("AE GeoIT Link");
                 var externalDefinition = definition as ExternalDefinition;
                 guid = externalDefinition.GUID;
-                Console.WriteLine(guid);
+                // Console.WriteLine(guid);
             }
             catch (Exception)
             {
@@ -36,31 +37,33 @@ namespace MyRevitCommands
             {
                 var wsParameters = excelEngine.Workbook.Worksheets.Add("Parameters");
 
+                // Add headers for excel file 
                 wsParameters.Cells[1, 1].Value = "Family";
                 wsParameters.Cells[1, 2].Value = "FamilySymbol";
                 wsParameters.Cells[1, 3].Value = "GeoIT Value";
-                wsParameters.Cells[1, 4].Value = "Unique ID";
 
+                // Filtering for every family type that is present in the current project. 
                 var familyTypeCollector1 =
                     new FilteredElementCollector(doc);
                 familyTypeCollector1.OfClass(typeof(ElementType));
                 var famTypes = familyTypeCollector1.ToElements();
 
+                // Excel starts counting at 1 so the first row with data filled in is 2
                 var row = 2;
-                var cellValue = 0;
+                // var cellValue = 0;
 
+
+                // start transaction 
                 using (var t = new Transaction(doc))
                 {
                     t.Start("Symbol activate");
 
+
+                    // Loops through all the family types that are present
                     foreach (var e in famTypes)
                     {
+                        // Changes element to elementType for access to parameters.
                         var type = e as ElementType;
-                        //if (!e.IsActive)x 
-                        //{
-                        //    e.Activate();
-                        //    doc.Regenerate();
-                        //}
 
                         // Got builtinparameters value through RevitLookUp .AsString is necessary to get value instead of DB.Parameter
                         wsParameters.Cells[row, 1].Value =
@@ -69,14 +72,17 @@ namespace MyRevitCommands
                             type.get_Parameter(BuiltInParameter.ALL_MODEL_TYPE_NAME).AsString();
                         var number = 0;
 
+                        // Parameter can be empty because some system families and types do not allow being changed
                         if (e.get_Parameter(guid) != null)
                         {
+                            // Changes excel from standard to number, this is done for importing.
                             int.TryParse(e.get_Parameter(guid).AsValueString(), out number);
                             wsParameters.Cells[row, 3].Value = number;
                             wsParameters.Cells["C1:C" + wsParameters.Dimension.End.Row].Style.Numberformat.Format = "0";
                         }
                         else
                         {
+                            // if null fill in 0
                             wsParameters.Cells[row, 3].Value = 0;
                         }
 
@@ -88,11 +94,14 @@ namespace MyRevitCommands
 
                 var fi = "";
 
+                // SaveFileDialog to save excel
                 var dlg = new WinForms.SaveFileDialog();
                 dlg.InitialDirectory = @"C:\";
                 dlg.Title = "Select directory to save the excel file";
                 dlg.Filter = "Excel spreadsheet files (*.xlsx)|*.xlsx|All files (*)|*";
 
+
+                // save file ok 
                 if (dlg.ShowDialog() == WinForms.DialogResult.OK) fi = dlg.FileName;
 
                 try
@@ -101,7 +110,7 @@ namespace MyRevitCommands
                 }
                 catch (Exception e)
                 {
-                    Console.WriteLine(e);
+                    //Console.WriteLine(e);
                     return Result.Cancelled;
                 }
             }
--- ImportParameter.cs
+++ ImportParameter.cs
@@ -21,12 +21,14 @@ namespace MyRevitCommands
             var doc = uiapp.ActiveUIDocument.Document;
             var row = 2;
 
+            // Gets shared parameter through guid 
             var file = app.OpenSharedParameterFile();
             var group = file.Groups.get_Item("00. General");
             var definition = group.Definitions.get_Item("AE GeoIT Link");
             var externalDefinition = definition as ExternalDefinition;
             guid = externalDefinition.GUID;
 
+            // OpenFileDialog to choose excel file
             var dlg = new WinForms.OpenFileDialog();
             dlg.Title = "Select source Excel file from which to update Revit shared parameters";
             dlg.Filter = "Excel spreadsheet files (*.xls;*.xlsx)|*.xls;*.xlsx|All files (*)|*";
@@ -35,13 +37,17 @@ namespace MyRevitCommands
 
             using (var excelEngine = new ExcelPackage(filename))
             {
+                // Only one worksheet is present. 
                 worksheet = excelEngine.Workbook.Worksheets[1];
+
+                // End row count, number to when stop looping
                 var iRowCnt = worksheet.Dimension.End.Row;
 
                 using (var t = new Transaction(doc))
                 {
                     t.Start("started");
 
+                    // Same filter as export, loops through every element(type)
                     var familyTypeCollector1 = new FilteredElementCollector(doc);
                     familyTypeCollector1.OfClass(typeof(ElementType));
                     var famTypes = familyTypeCollector1.ToElements();
@@ -60,14 +66,12 @@ namespace MyRevitCommands
         {
             // Debug.WriteLine("BEGIN");
             var row = 2;
-            var fstype = type;
             string famname = null;
             string famsymbol = null;
 
-            // if (!type.IsActive) type.Activate();
-
             if (type != null)
             {
+                // Uses builtinparameters to not encounter errors or other parameters values with the same names
                 famname = type.get_Parameter(BuiltInParameter.ALL_MODEL_FAMILY_NAME).AsString();
                 famsymbol = type.get_Parameter(BuiltInParameter.ALL_MODEL_TYPE_NAME).AsString();
             }
@@ -75,47 +79,26 @@ namespace MyRevitCommands
             var run = true;
             while (run && row < iRowCnt)
             {
+                // Reads excel values for each row
                 var excelfamily = (string) worksheet.Cells[row, 1].Value;
                 var excelfs = (string) worksheet.Cells[row, 2].Value;
                 var gvalue = (double) worksheet.Cells[row, 3].Value;
 
+                // Compares actual famname and fs name to excel values 
                 if (famname.Equals(excelfamily))
                     if (famsymbol.Equals(excelfs))
                     {
                         var fspara = type.get_Parameter(guid);
 
                         if (fspara != null && fspara.IsReadOnly != true)
-                        {
+                            // Writes value
                             fspara.Set(gvalue);
-                            //Debug.WriteLine("OK");
-                        }
 
                         run = false;
                     }
 
                 row++;
             }
-
-
-/*
-            var excelfamily = (string) worksheet.Cells[row, 1].Value;
-            var excelfs = (string) worksheet.Cells[row, 2].Value;
-            var gvalue = (double) worksheet.Cells[row, 3].Value;
-
-            var skip = false;
-            if (famname.Equals(excelfamily))
-                if (famsymbol.Equals(excelfs))
-                {
-                    var fspara = type.get_Parameter(guid);
-                    if (fspara == null)
-                        skip = true;
-                    else if (fspara.IsReadOnly != true)
-                        fspara.Set(gvalue);
-                }
-
-            if (!skip && row + 1 < iRowCnt && (famname != excelfamily || famsymbol != excelfs))
-                checkFamily(row + 1, iRowCnt, type);
-*/
         }
     }
 }
